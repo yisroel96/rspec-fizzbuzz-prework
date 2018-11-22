24af9bb58371946597ba3ab2af29eaf776047433
witty-collection-3981 <yisroel96@gmail.com>
2018-11-22 17:44:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
-  elsif int % 3 == 0 && int % 5 == 0
-      "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   end
